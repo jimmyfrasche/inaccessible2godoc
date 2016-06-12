1786b41baddc0dedb153989673fa5a0140aba893
jimmyfrasche <soapboxcicero@gmail.com>
2016-06-12 14:26:22 -0700

    0

---
+++ examples.go
@@ -0,0 +1,98 @@
+//Package inaccessible2godoc demonstrates all the cases
+//where an unexported type is part of the public API of a package
+//but is not shown by godoc.
+//
+//It does not actually do anything.
+package inaccessible2godoc
+
+//unexportedProperly should only be shown when ?m=all.
+//Everything else should be in some way accessible by default.
+var unexportedProperly = 0
+
+//unexportedButUsedInAVar is not accessible.
+type unexportedButUsedInAVar struct{}
+
+//Foo is inaccessible.
+func (unexportedButUsedInAVar) Foo() {}
+
+//ExportedVarOfUnexportedButUsedInAVar is exported but the docs and methods of its type are inaccessible.
+var ExportedVarOfUnexportedButUsedInAVar = unexportedButUsedInAVar{}
+
+//unexportedButUsedInAConst is not accessible.
+type unexportedButUsedInAConst int
+
+//Foo is inaccessible.
+func (unexportedButUsedInAConst) Foo() {}
+
+//ExportedConstOfUnexportedButUsedInAConst is exported but the docs and methods of its type are inaccessible.
+const ExportedConstOfUnexportedButUsedInAConst = unexportedButUsedInAConst(0)
+
+//unexportedButUsedInAField is not accessible.
+type unexportedButUsedInAField struct{}
+
+//Foo is inaccessible.
+func (unexportedButUsedInAField) Foo() {}
+
+//UsesUnexportedInAField uses an unexported type in a field.
+type UsesUnexportedInAField struct {
+	//Nodocs is exported but the docs and methods of its type are inaccessible and unlinked.
+	Nodocs unexportedButUsedInAField
+}
+
+//unexportedButEmbedded is not accessible.
+type unexportedButEmbedded struct {
+	//Exported is not accessible.
+	Exported int
+}
+
+//Foo is inaccessible.
+func (unexportedButEmbedded) Foo() {}
+
+//EmbedsUnexportedType embeds an unexported type that contains exported methods and fields which are not listed.
+type EmbedsUnexportedType struct {
+	unexportedButEmbedded
+}
+
+//Bar is accessible, but Foo is not.
+func (EmbedsUnexportedType) Bar() {}
+
+//unexportedButEmbeddedInterface is not accessible.
+type unexportedButEmbeddedInterface interface {
+	Foo()
+}
+
+//EmbedsUnexportedInterface embeds an unexported interface.
+//
+//Every inaccessible type in this package satisfies this interface, but you wouldn't know by looking.
+type EmbedsUnexportedInterface interface {
+	unexportedButEmbeddedInterface
+}
+
+//unexportedButReturned is not accessible.
+type unexportedButReturned struct{}
+
+//Foo is inaccessible.
+func (unexportedButReturned) Foo() {}
+
+//ReturnUnexportedButReturned returns an unexported type which is inaccessible.
+func ReturnUnexportedButReturned() *unexportedButReturned {
+	return nil
+}
+
+//unexportedButUsedInParameter is not accessible.
+type unexportedButUsedInParameter int
+
+//Foo is inaccessible.
+func (unexportedButUsedInParameter) Foo() {}
+
+//UsesUnexportedAsParameter takes an unexported type as a parameter.
+func UsesUnexportedAsParameter(a unexportedButUsedInParameter) {}
+
+var (
+	_ EmbedsUnexportedInterface = unexportedButUsedInAVar{}
+	_ EmbedsUnexportedInterface = unexportedButUsedInAConst(0)
+	_ EmbedsUnexportedInterface = unexportedButUsedInAField{}
+	_ EmbedsUnexportedInterface = unexportedButEmbedded{}
+	_ EmbedsUnexportedInterface = unexportedButReturned{}
+	_ EmbedsUnexportedInterface = unexportedButUsedInParameter(0)
+)
